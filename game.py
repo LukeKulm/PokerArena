ad0de20443a17c35f5a6a2722a206494aab22608
Max Whitton <mmw243@cornell.edu>
2024-10-17 21:45:25 -0400

    hand class

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
 # simulates a game of player objects and makes repeated calls to score_hand
+
+Class Hand:
+    def __init__(self):
+        self.nums = []
+        self.suits = []
+    def add_card(self, num, suit):
+        self.nums+=num
+        self.suits+=suit
+    
