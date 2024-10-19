a1b70adcf96a0e7c82853858bcc58f2e632970e9
Cole Breen <ctb93@cornell.edu>
2024-10-19 15:11:46 -0400

    Added new stuff to player

--- player.py
+++ player.py
@@ -2,17 +2,23 @@
 
 import random
 from abc import ABC, abstractmethod
+import simulate_games
+
 
 class Player(ABC):
-    
+
     # returns a tuple (move, ammount) where move is 0 for a fold, 1 for a check, 2 for a bet
     @abstractmethod
     def act(self, game):
         pass
+
     def bet(self, ammount):
-        self.balance-=ammount
+        self.balance -= ammount
+
     def win(self, amm):
-        self.balance+=amm
+        self.balance += amm
+
+
 def get_suit(num):
     if num == 0:
         return "Hearts"
@@ -22,8 +28,10 @@ def get_suit(num):
         return "Clubs"
     else:
         return "Spades"
+
+
 def get_rank(num):
-    if num <11:
+    if num < 11:
         return str(num)
     else:
         if num == 11:
@@ -34,15 +42,19 @@ def get_rank(num):
             return "King"
         else:
             return "Ace"
+
+
 def decode_cards(lis):
     ans = ""
     for i in range(0,  len(lis), 2):
         if lis[i] != 0:
-            ans+= get_rank(lis[i])
-            ans+=" "
-            ans+= get_suit(lis[i+1])
-            ans+=", "
+            ans += get_rank(lis[i])
+            ans += " "
+            ans += get_suit(lis[i+1])
+            ans += ", "
     return ans
+
+
 def decode_stage(num):
     if num == 0:
         return "Preflop"
@@ -53,38 +65,40 @@ def decode_stage(num):
     else:
         return "River"
 
+
 class Human(Player):
     def __init__(self, balance):
         self.balance = balance
         self.folded = False
         self.allin = False
+
     def act(self, state):
         n = state[0]
-        i  = state[1]
-        hand =  decode_cards(state[2:6])
-        idealer =  state[6]
-        stage =   decode_stage(state[7])
-        in_hand  = state[8]
+        i = state[1]
+        hand = decode_cards(state[2:6])
+        idealer = state[6]
+        stage = decode_stage(state[7])
+        in_hand = state[8]
         pot = state[9]
         board = decode_cards(state[10:20])
         stack = state[20]
         bet = state[21] - state[22]
-        
+
         # if  i  ==  idealer:
         #     print("You are the dealer.")
         # elif i>idealer:
         #     print("You are in the "+i-idealer+" position.")
         # else:
         #     print("You are in the "+n-(idealer-i)+" position.")
-        
+
         print("\nIt is player "+str(i)+"'s turn.")
         print("You have "+str(stack)+" chips in your stack.")
         print("The stage of the hand is "+stage)
-        if stage!= "Preflop":
+        if stage != "Preflop":
             print("The board is "+board[:-2]+".")
         print("Your hand is "+hand[:-2]+".")
         print("There is "+str(pot)+" in the pot.")
-        
+
         if bet == 0:
             move = "x"
             while move not in "fcr":
@@ -95,15 +109,15 @@ class Human(Player):
                 return (1, 0, 0)
             else:
                 amm = 0
-                while int(amm) <=0 or int(amm) > self.balance:
+                while int(amm) <= 0 or int(amm) > self.balance:
                     amm = int(input("how much do you want to raise by: "))
-                
+
                 if amm < self.balance:
                     return (2, amm, 0)
                 else:
                     self.allin = True
-                    return  (2, self.balance, 1)
-            
+                    return (2, self.balance, 1)
+
         else:
             move = "x"
             print("It is "+str(bet)+" to call.")
@@ -112,56 +126,61 @@ class Human(Player):
             if move == 'f':
                 return (0, 0,  0)
             elif move == 'c':
-                if bet<self.balance:
+                if bet < self.balance:
                     return (1, bet, 0)
                 else:
                     self.allin = True
                     return (1, self.balance, 1)
             else:
                 amm = 0
-                while int(amm) <=0 or int(amm) > self.balance or int(amm)<2*bet:
+                while int(amm) <= 0 or int(amm) > self.balance or int(amm) < 2*bet:
                     amm = int(input("how much do you want to raise by: "))
                 if amm < self.balance:
                     return (2, amm, 0)
                 else:
                     self.allin = True
-                    return  (2, self.balance, 1)
-                
+                    return (2, self.balance, 1)
+
+
 class Random(Player):
     def __init__(self, balance):
         self.balance = balance
         self.folded = False
         self.allin = False
+
     def act(self, state):
         bet = state[21]
         if bet == 0:
-            move = random.choice(['c', 'r']) #randomly choose check or raise
+            move = random.choice(['c', 'r'])  # randomly choose check or raise
             if move == 'c':
                 return (1, 0, 0)
             else:
-                amm = random.randint(0, self.balance) #randomly select amount below balance
+                # randomly select amount below balance
+                amm = random.randint(0, self.balance)
                 if amm < self.balance:
                     return (2, amm, 0)
                 else:
-                    return  (2, self.balance, 1)
-            
+                    return (2, self.balance, 1)
+
         else:
-            move = random.choice(['f', 'c', 'r']) #randomly choose one of the three moves
+            # randomly choose one of the three moves
+            move = random.choice(['f', 'c', 'r'])
             if move == 'f':
                 return (0, 0,  0)
             elif move == 'c':
-                if bet<self.balance:
+                if bet < self.balance:
                     return (1, bet, 0)
                 else:
                     self.allin = True
                     return (1, self.balance, 1)
             else:
-                amm = random.randint(0, self.balance) #randomly select amount below balance
+                # randomly select amount below balance
+                amm = random.randint(0, self.balance)
                 if amm < self.balance:
                     return (2, amm, 0)
                 else:
                     self.allin = True
-                    return  (2, self.balance, 1)
+                    return (2, self.balance, 1)
 
 
 class MonteCarloAgent(Player):
@@ -171,6 +190,13 @@ class MonteCarloAgent(Player):
         self.allin = False
 
         self.number_of_opps = number_of_opps
+
     def act(self, state):
         bet = state[21]
-    
+        win_rate = simulate_games.expected_win_rate(
+            state[2:6], state[10:20], self.number_of_opps)
+        if win_rate > 0.50:
+            self.allin = True
+            return (2, self.balance, 1)
+        else:
+            return (0, 0, 0)
