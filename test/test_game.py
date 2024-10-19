54d7530dff2708f1708bb8ee51f9dcc8afe75050
lbk73 <lbk73@cornell.edu>
2024-10-19 13:29:48 -0400

    working on tests for game

--- test/test_game.py
+++ test/test_game.py
@@ -3,9 +3,17 @@ import game
 
 
 class TestGameSteps:
-    #make sure if everyone folds then last wins
-    def test_game_step(self):
-      g = game.Game(3)
-      g.step()
+    def test_game_step_random_players(self):
+      #make sure game doesnt crash when run 100 times
+      g = game.Game(["Random", "Random"], 200)
+      for i in range(100):
+        g.step()
+      assert game == game
+      
+    def test_everyone_folds_last_wins(self):
+      g = game.Game([["Random", "Random", "Random"], 200])
+      g.deal_hole_cards()
+      g.stage = 1
+      #TODO: finish this
       assert game == game
         
