6851890dae89cde0830fe928a5ab68ffbe5825c9
Cole Breen <ctb93@cornell.edu>
2024-11-24 12:45:24 -0500

    universal_card_functions tests

--- test/test_universal_card_functions.py
+++ test/test_universal_card_functions.py
@@ -9,3 +9,27 @@ def test_rank_to_num():
     assert universal_card_functions.rank_to_num("A") == 14
     for i in range(2, 10):
         assert universal_card_functions.rank_to_num(str(i)) == i
+
+
+def test_suit_to_num():
+    assert universal_card_functions.suit_to_num("c") == 0
+    assert universal_card_functions.suit_to_num("d") == 1
+    assert universal_card_functions.suit_to_num("h") == 2
+    assert universal_card_functions.suit_to_num("s") == 3
+
+
+def test_num_to_rank():
+    assert universal_card_functions.num_to_rank(10) == "T"
+    assert universal_card_functions.num_to_rank(11) == "J"
+    assert universal_card_functions.num_to_rank(12) == "Q"
+    assert universal_card_functions.num_to_rank(13) == "K"
+    assert universal_card_functions.num_to_rank(14) == "A"
+    for i in range(2, 10):
+        assert universal_card_functions.num_to_rank(i) == str(i)
+
+
+def test_num_to_suite():
+    assert universal_card_functions.num_to_suite(0) == "c"
+    assert universal_card_functions.num_to_suite(1) == "d"
+    assert universal_card_functions.num_to_suite(2) == "h"
+    assert universal_card_functions.num_to_suite(3) == "s"
