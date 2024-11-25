440d8e085063ba821bb60183a5326cb28853cce4
Luke Kulm <kulmluke@gmail.com>
2024-11-25 16:20:30 -0500

    added more tests to pluribus extraction and added test to test all agents in a big game

---
+++ test/test_agents.py
@@ -0,0 +1,29 @@
+import pytest
+import sys
+import game
+import os
+
+from contextlib import redirect_stdout
+from scripts.utils import get_not_busted
+
+
+def test_all_ai_agents():
+  players = [("MonteCarloQLearningHybrid", "saved_models/q_network_with_montecarlo.pth"), 
+             ("QLearningAgent", "saved_models/q_network.pth"), 
+             ("MonteCarlo", ""), 
+             ("Random", ""),
+             ("BCPlayer", ""),
+             ("SmartBCPlayer", "")]
+  i = 0
+  games = 0
+
+  with open(os.devnull, 'w') as fnull:
+      with redirect_stdout(fnull):
+
+          n = 1000
+          while i < n:
+              g = game.Game(players, 200)
+              games += 1
+              while i < n and get_not_busted(g, 2) == len(players) and not g.user_ended:
+                  g.step()
+                  i += 1
--- test/test_pluribus_extraction.py
+++ test/test_pluribus_extraction.py
@@ -19,7 +19,9 @@ def test_determine_stage():
   assert determine_stage(community_cards) == 3
   
 def test_determine_dealer_position():
-  return 0
+  actions = ['d dh p1 6d5s', 'd dh p2 Td2d', 'd dh p3 4dKh', 'd dh p4 7c4c', 'd dh p5 3s6c', 'd dh p6 7d8c', 'p3 f', 'p4 f', 'p5 f', 'p6 cbr 250', 'p1 f', 'p2 cc', 'd db 6sAsTh', 'p2 cc', 'p6 cbr 275', 'p2 cc', 'd db 7s', 'p2 cc', 'p6 cc', 'd db Ts', 'p2 cc', 'p6 cc', 'p2 sm Td2d', 'p6 sm']
+  num_players = 6
+  assert determine_dealer_position(actions, num_players) == 1 # could be 2, also dealer position never changes
   
 def test_encode_hand_history():
   actions = ['d dh p1 3c9s', 'd dh p2 6d5s', 'd dh p3 9dTs', 'd dh p4 2sQs', 'd dh p5 AdKd', 'd dh p6 7cTc', 'p3 f', 'p4 f', 'p5 cbr 225', 'p6 f', 'p1 f', 'p2 f']
