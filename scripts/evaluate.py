2182c1f2eedccb264c705841e10e0b3593baf585
Cole Breen <ctb93@cornell.edu>
2024-12-17 21:00:41 -0500

    Updated evaluate script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -16,8 +16,6 @@ def main(advanced_tracking: bool):
     players = [("QLearningAgent",
                 "saved_models/q_learning_agent.pth"),
                 ("MonteCarloQLearningHybrid", "saved_models/montecarlo_q_hybrid.pth"), 
-                ("QLearningAgent",
-                "saved_models/q_network.pth"),
                 ("Random", None),
                 ("Random", None)]
     
